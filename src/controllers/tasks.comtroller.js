99b2ad93d55502807e3fa489da0dd9eb3bbdd32d
Gerardo Miranda <gerardodev25@gmail.com>
2022-03-10 20:25:22 -0400

    metodo post crear tarea

--- src/controllers/tasks.comtroller.js
+++ src/controllers/tasks.comtroller.js
@@ -1,3 +1,4 @@
+import db from '../database';
 import { response, request } from 'express';
 
 export const getAllTasks = async (req = request, res = response) => {
@@ -9,12 +10,22 @@ export const getOneTask = async (req = request, res = response) => {
 };
 
 export const createTask = async (req = request, res = response) => {
-  res.send('creating a task');
+  try {
+    const { title, descriptin } = req.body;
+
+    const result = await db.query('insert into task (title, descriptin) values($1, $2) returning *', [title, descriptin]);
+
+    console.log(result);
+    res.json({ roes: result.rows[0] });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ error: error.message });
+  }
 };
 export const updateTask = async (req = request, res = response) => {
   res.send('updating a task');
-}
+};
 
 export const deleteTask = async (req = request, res = response) => {
   res.send('deleting a task');
-}
+};
